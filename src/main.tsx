97ebedf4d4cbcf0b655e3a8feb7769e8cc62df6c
Amir Daneshmand <amirdanesh02@gmail.com>
2025-10-06 03:40:08 +0330

    feat(main.tsx): provide error handling feature in the main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import { GlobalErrorHandler, ErrorBoundary } from "@/providers/ErrorBoundary";
 import { router } from "./router";
 import "@/styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <GlobalErrorHandler>
+      <ErrorBoundary>
+        <RouterProvider router={router} />
+      </ErrorBoundary>
+    </GlobalErrorHandler>
   </StrictMode>
 );

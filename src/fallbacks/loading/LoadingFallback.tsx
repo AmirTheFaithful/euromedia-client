982e9f4660cc388b09ae844dda42f93633cbb3b2
Amir Daneshmand <amirdanesh02@gmail.com>
2025-10-08 01:39:54 +0330

    feat(fallbacks/loading): Add loading fallback page

---
+++ src/fallbacks/loading/LoadingFallback.tsx
@@ -0,0 +1,21 @@
+import type { JSX, FC } from "react";
+
+import E from "@/icons/e.svg?react";
+import U from "@/icons/u.svg?react";
+import R from "@/icons/r.svg?react";
+import O from "@/icons/o.svg?react";
+
+import styles from "./loadingFallback.module.scss";
+
+export const LoadingFallback: FC = (): JSX.Element => {
+  return (
+    <main className={styles.page}>
+      <div className={styles.signature}>
+        <E />
+        <U />
+        <R />
+        <O />
+      </div>
+    </main>
+  );
+};

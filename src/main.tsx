f605324d28f18c3709a0a4806ed660ebb624b65f
Amir Daneshmand <amirdanesh02@gmail.com>
2025-10-22 18:28:58 +0330

    feat(main.tsx): provide StoreProvider for global state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider as StoreProvider } from "react-redux";
 
 import { GlobalErrorHandler, ErrorBoundary } from "@/providers/ErrorBoundary";
 import { router } from "./router";
+import store from "@/app/store";
 import "@/styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GlobalErrorHandler>
       <ErrorBoundary>
-        <RouterProvider router={router} />
+        <StoreProvider store={store}>
+          <RouterProvider router={router} />
+        </StoreProvider>
       </ErrorBoundary>
     </GlobalErrorHandler>
   </StrictMode>

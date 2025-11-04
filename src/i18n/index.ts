d0c1fb5650d4ba2e9649401c3e513f0d2d73697e
Amir Daneshmand <amirdanesh02@gmail.com>
2025-11-04 15:50:50 +0330

    feat(i18n): setup i18n

---
+++ src/i18n/index.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import enCommon from "./locales/en/common.json";
+import enHome from "./locales/en/home.json";
+import enAuth from "./locales/en/auth.json";
+
+void i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { common: enCommon, home: enHome, auth: enAuth },
+    },
+    fallbackLng: "en",
+    lng: "en",
+    ns: ["common", "auth"],
+    defaultNS: "common",
+    interpolation: { escapeValue: false },
+    detection: {
+      order: ["localStorage", "navigation"],
+      caches: ["localStorage"],
+    },
+  });
+
+export default i18n;

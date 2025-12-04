4549f61a8959bedd9cbf48345526f7af60265fc7
Amir Daneshmand <amirdanesh02@gmail.com>
2025-12-04 21:25:29 +0330

    feat(jest.config.js): register SVG and images mocks into jest.config's moduleNameMapper object

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,12 @@ export default {
     ...tsJestTransformCfg,
   },
   moduleNameMapper: {
+    "\\.module\\.(css|scss)$": "identity-obj-proxy",
     "\\.(css|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
-    "\\.svg\\?react$": "<rootDir>/src/__mocks__/svgMock.js",
+    "\\.svg(\\?react)?$": "<rootDir>/src/__mocks__/svgMock.js",
+    "\\.(jpg|jpeg|png|webp|gif)$": "<rootDir>/src/__mocks__/fileMock.js",
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   transformIgnorePatterns: ["/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 };
